cdb63f67965b471d18eb432761bbb0b2892bd2c1
Tita Wijayanti <wijayantitita094@gmail.com>
2021-02-16 16:06:23 +0700

    02_praktikum_web_lanjut_dua

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +27,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 /***Praktikum 2 */
-Route::get('/',  [PageController::class,'index']);
-Route::get('/about',  [PageController::class,'about']);
-Route::get('/articles/{id}',  [PageController::class,'articles']);
+Route::get('/',  [HomeController::class,'index']);
+Route::get('/about',  [AboutController::class,'about']);
+Route::get('/articles/{id}',  [ArticleController::class,'articles']);
